d80d1b8ca5460cc9434e06ffafe07fd09540d4fc
rnpacifique <rpacfique12@gmail.com>
2024-06-06 19:57:58 +0200

    task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a type-annotated function concat that takes a string str1 and a string
+str2 as arguments and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenates two strings"""
+    return str1 + str2
