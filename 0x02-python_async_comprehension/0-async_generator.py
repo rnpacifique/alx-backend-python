f86fdd5e3eda8c3655c85e92160ee4fa9aeee473
rnpacifique <rpacfique12@gmail.com>
2024-06-13 20:45:44 +0200

    tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""The coroutine will loop 10 times, each time asynchronously wait 1 second,
-then yield a random number between 0 and 10"""
-
-
+"""This is a module  for creating an asynchronous generator."""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """Coroutine that yields random numbers asynchronously"""
-    for _ in range(10):
+async def async_generator() -> AsyncGenerator[float, None]:
+    """This coroutine asynchronously yields random floats."""
+    n: int = 1
+    while n <= 10:
         await asyncio.sleep(1)
-        yield random.random() * 10s
+        yield random.uniform(0, 10)
+        n += 1
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""The coroutine will collect 10 random numbers using an async comprehensing
-over async_generator, then return the 10 random numbers"""
+'''Import async_generator from the previous task and then write
+a coroutine called async_comprehension that takes no arguments.
+'''
 
-
-import asyncio
-from typing import List
-from importlib import import_module as using
-
+import typing
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """Coroutine that collects 10 random numbers asynchronously"""
-    return [num async for num in async_generator()]
+async def async_comprehension() -> typing.List[float]:
+    '''This creates a list of 10 numbers from a 10-number generator.
+    '''
+    return [rand async for rand in async_generator()]
